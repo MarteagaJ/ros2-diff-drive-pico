c6062823b93de4fabeff407b479830b3f149873a
MarteagaJ <mjarteaga2019@gmail.com>
2023-03-30 17:18:43 -0400

    override the proper functions that are virtual

--- diff-drive-pico/include/diff-drive-pico/diff-drive-pico.hpp
+++ diff-drive-pico/include/diff-drive-pico/diff-drive-pico.hpp
@@ -2,7 +2,32 @@
 
 namespace diff_drive_pico
 {
-    class DiffDrivePico : public hardware_interface::ActuatorInterface {
-        
+    class DiffDrivePico : public hardware_interface::ActuatorInterface 
+    {
+
+    public:
+        DiffDrivePico(){}
+
+        CallbackReturn on_configure(const State & previous_state);
+
+        CallbackReturn on_cleanup(const State & previous_state);
+
+        CallbackReturn on_shutdown(const State & previous_state);
+
+        CallbackReturn on_activate(const State & previous_state);
+
+        CallbackReturn on_deactivate(const State & previous_state);
+
+        CallbackReturn on_error(const State & previous_state);
+
+        // CallbackReturn on_init(const HardwareInfo & hardware_info);
+
+        std::vector<StateInterface> export_state_interfaces();
+
+        std::vector<CommandInterface> export_command_interfaces();
+
+        return_type read(const rclcpp::Time & time, const rclcpp::Duration & period);
+
+        return_type write(const rclcpp::Time & time, const rclcpp::Duration & period);
     }
 }
